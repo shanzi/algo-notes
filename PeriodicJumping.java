305bb086ff537d323ed9d4ed66f901b5276a8be4
chase <chase@local>
2015-04-16 13:24:49 +0800

    update periodicjumping

--- PeriodicJumping.java
+++ PeriodicJumping.java
@@ -1,14 +1,14 @@
 public class PeriodicJumping {
-	long sum(int[] jumpLengths) {
-		long total = 0;
-		for (int i : jumpLengths) total += i;
-		return total;
-	}
+    long sum(int[] jumpLengths) {
+        long total = 0;
+        for (int i : jumpLengths) total += i;
+        return total;
+    }
     public int minimalTime(int x, int[]jumpLengths) {
-		x = Math.abs(x);
-		long total = sum(jumpLengths);
-		if (x % total == 0) return (int)(x / total);
-		
+        x = Math.abs(x);
+        long total = sum(jumpLengths);
+        if (x % total == 0) return (int)(x / total);
+        
         int steps = 0;
         long min = 0;
         long max = 0;
@@ -22,11 +22,11 @@ public class PeriodicJumping {
             long minl = min - stepLen;
             long minr = max - stepLen;
             if ((minl ^ minr) >= 0) {
-				min = Math.min(Math.abs(minl), Math.abs(minr));
-			} else {
-				min = 0;
-			}
-			max += stepLen;
+                min = Math.min(Math.abs(minl), Math.abs(minr));
+            } else {
+                min = 0;
+            }
+            max += stepLen;
             steps++;
         }
 
