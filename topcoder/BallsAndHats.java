9a18aa0237e8ad465a4dd05ea30bb93b59c40f55
Chase Zhang <yun.er.run@gmail.com>
2015-05-13 11:16:31 +0800

    add BallsAndHats

---
+++ topcoder/BallsAndHats.java
@@ -0,0 +1,14 @@
+public class BallAndHats {
+    public int getHat(String hats, int numSwaps) {
+        if (numSwaps == 0) {
+            for (int i = 0; i < 3; i++) {
+                if (hats.charAt(i) == 'o') return i;
+            }
+        }
+        if (hats.charAt(0) == 'o' || hats.charAt(2) == 'o') {
+            return numSwaps % 2;
+        } else {
+            return 1 - (numSwaps % 2);
+        }
+    }
+}
